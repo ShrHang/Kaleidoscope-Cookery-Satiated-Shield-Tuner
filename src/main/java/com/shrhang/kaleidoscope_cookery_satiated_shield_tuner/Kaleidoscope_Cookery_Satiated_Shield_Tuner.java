4fe9c8a5ba48ea3f354318573e8bac9f9c3e27ca
ShrHang <shhshrhang@gmail.com>
2026-04-02 17:05:54 +0800

    添加了配置屏幕

--- src/main/java/com/shrhang/kaleidoscope_cookery_satiated_shield_tuner/Kaleidoscope_Cookery_Satiated_Shield_Tuner.java
+++ src/main/java/com/shrhang/kaleidoscope_cookery_satiated_shield_tuner/Kaleidoscope_Cookery_Satiated_Shield_Tuner.java
@@ -1,15 +1,22 @@
 package com.shrhang.kaleidoscope_cookery_satiated_shield_tuner;
 
 import com.shrhang.kaleidoscope_cookery_satiated_shield_tuner.content.config.ServerConfig;
+import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
+import net.neoforged.fml.loading.FMLEnvironment;
+import net.neoforged.neoforge.client.gui.ConfigurationScreen;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 
 @Mod(Kaleidoscope_Cookery_Satiated_Shield_Tuner.MODID)
 public class Kaleidoscope_Cookery_Satiated_Shield_Tuner {
     public static final String MODID = "kaleidoscope_cookery_satiated_shield_tuner";
     public Kaleidoscope_Cookery_Satiated_Shield_Tuner(IEventBus modEventBus, ModContainer modContainer) {
-        modContainer.registerConfig(ModConfig.Type.SERVER, ServerConfig.SPEC);
+        modContainer.registerConfig(ModConfig.Type.SERVER, ServerConfig.serverSpec);
+        if (FMLEnvironment.dist == Dist.CLIENT) {
+            modContainer.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
+        }
     }
 }
--- src/main/java/com/shrhang/kaleidoscope_cookery_satiated_shield_tuner/content/config/ServerConfig.java
+++ src/main/java/com/shrhang/kaleidoscope_cookery_satiated_shield_tuner/content/config/ServerConfig.java
@@ -4,7 +4,7 @@ import net.neoforged.neoforge.common.ModConfigSpec;
 
 public class ServerConfig {
     private static final ModConfigSpec.Builder builder = new ModConfigSpec.Builder();
-    public static ModConfigSpec SPEC;
+    public static ModConfigSpec serverSpec;
 
     public static final ModConfigSpec.BooleanValue IS_MODIFY_SATIATED_SHIELD;
     public static final ModConfigSpec.BooleanValue IS_SATIATED_SHIELD_DISABLE_WHEN_HUNGRY_EFFECT;
@@ -25,11 +25,13 @@ public class ServerConfig {
                         "是否启用饱腹代偿效果削弱。",
                         "如果《森罗物语：兼容》的饱腹代偿效果削弱被启用，则优先使用它的削弱效果。且本模组的配置项无效。"
                 )
+                .translation("kaleidoscope_cookery_satiated_shield_tuner.configgui.is_modify_satiated_shield")
                 .define("IS_MODIFY_SATIATED_SHIELD", true);
 
         IS_SATIATED_SHIELD_DISABLE_WHEN_HUNGRY_EFFECT = builder
                 .comment("If true, the Satiated Shield effect will not apply while the player has the Hunger effect.")
                 .comment("当为 true 时，玩家存在饥饿状态效果时饱腹代偿效果不生效。")
+                .translation("kaleidoscope_cookery_satiated_shield_tuner.configgui.is_satiated_shield_disable_when_hungry_effect")
                 .define("IS_SATIATED_SHIELD_DISABLE_WHEN_HUNGRY_EFFECT", true);
 
         SATIATED_SHIELD_MIN_FOOD_LEVEL = builder
@@ -41,6 +43,7 @@ public class ServerConfig {
                         "饱腹代偿生效所需的最小饥饿值（整数）。",
                         "当玩家饥饿值低于该值时，饱腹代偿不生效。"
                 )
+                .translation("kaleidoscope_cookery_satiated_shield_tuner.configgui.satiated_shield_min_food_level")
                 .defineInRange("SATIATED_SHIELD_MIN_FOOD_LEVEL", 14, 1, 20);
 
         SATIATED_SHIELD_ADDITIONAL_EXHAUSTION_PER_DAMAGE = builder
@@ -54,6 +57,7 @@ public class ServerConfig {
                         "4点疲劳值 = 1点饱食度/饥饿值，在 1 游戏刻中玩家最多积累 40 点疲劳值。",
                         "例如：如果设置为 0.5，每点被减免的伤害将额外造成 0.5 点疲劳值。"
                 )
+                .translation("kaleidoscope_cookery_satiated_shield_tuner.configgui.satiated_shield_additional_exhaustion_per_damage")
                 .defineInRange("SATIATED_SHIELD_ADDITIONAL_EXHAUSTION_PER_DAMAGE", 2.0, 0.0, 40.0);
 
         SATIATED_SHIELD_DAMAGE_REDUCTION_PERCENT = builder
@@ -67,6 +71,7 @@ public class ServerConfig {
                         "玩家受到的<原始伤害>将乘以<伤害减免百分比>来计算<伤害减免量>。",
                         "例如：如果设置为 0.85，玩家将受到 15% 的原始伤害。"
                 )
+                .translation("kaleidoscope_cookery_satiated_shield_tuner.configgui.satiated_shield_damage_reduction_percent")
                 .defineInRange("SATIATED_SHIELD_DAMAGE_REDUCTION_PERCENT", 0.85, 0.0, 1.0);
 
         SATIATED_SHIELD_MAX_DAMAGE_REDUCTION = builder
@@ -80,6 +85,7 @@ public class ServerConfig {
                         "<伤害减免量>大于该值时，将被限制为该值。",
                         "例如：如果设置为 10.0，玩家受到的伤害将被限制为最多减少 10 点伤害。"
                 )
+                .translation("kaleidoscope_cookery_satiated_shield_tuner.configgui.satiated_shield_max_damage_reduction")
                 .defineInRange("SATIATED_SHIELD_MAX_DAMAGE_REDUCTION", 10.0, 0.0, Integer.MAX_VALUE);
 
         SATIATED_SHIELD_MIN_DAMAGE = builder
@@ -93,6 +99,7 @@ public class ServerConfig {
                         "<原始伤害>高于该值时，<最终伤害>将被限制为不低于该值。",
                         "<最终伤害> = <原始伤害> - <伤害减免量>"
                 )
+                .translation("kaleidoscope_cookery_satiated_shield_tuner.configgui.satiated_shield_min_damage")
                 .defineInRange("SATIATED_SHIELD_MIN_DAMAGE", 2.0, 0.0, Integer.MAX_VALUE);
 
         SATIATED_SHIELD_WEAKNESS_DAMAGE_MULTIPLIER = builder
@@ -104,8 +111,9 @@ public class ServerConfig {
                         "当玩家受到饱腹代偿弱点伤害时，每点伤害增加的疲劳值的乘算倍率。",
                         "4点疲劳值 = 1点饱食度/饥饿值，在 1 游戏刻中玩家最多积累 40 点疲劳值。"
                 )
+                .translation("kaleidoscope_cookery_satiated_shield_tuner.configgui.satiated_shield_weakness_damage_multiplier")
                 .defineInRange("SATIATED_SHIELD_WEAKNESS_DAMAGE_MULTIPLIER", 2.0, 1.0, Integer.MAX_VALUE);
 
-        SPEC = builder.build();
+        serverSpec = builder.build();
     }
 }
